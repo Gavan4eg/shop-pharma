63be8afc314392ed72245ce8a9f7f7c22a013cbe
Alex <alex@Alexs-MacBook-Air.local>
2025-02-14 11:46:59 +0100

    fix

--- js/app.js
+++ js/app.js
@@ -5463,6 +5463,258 @@
                 destroy
             });
         }
+        function Autoplay(_ref) {
+            let {swiper, extendParams, on, emit, params} = _ref;
+            swiper.autoplay = {
+                running: false,
+                paused: false,
+                timeLeft: 0
+            };
+            extendParams({
+                autoplay: {
+                    enabled: false,
+                    delay: 3e3,
+                    waitForTransition: true,
+                    disableOnInteraction: false,
+                    stopOnLastSlide: false,
+                    reverseDirection: false,
+                    pauseOnMouseEnter: false
+                }
+            });
+            let timeout;
+            let raf;
+            let autoplayDelayTotal = params && params.autoplay ? params.autoplay.delay : 3e3;
+            let autoplayDelayCurrent = params && params.autoplay ? params.autoplay.delay : 3e3;
+            let autoplayTimeLeft;
+            let autoplayStartTime = (new Date).getTime();
+            let wasPaused;
+            let isTouched;
+            let pausedByTouch;
+            let touchStartTimeout;
+            let slideChanged;
+            let pausedByInteraction;
+            let pausedByPointerEnter;
+            function onTransitionEnd(e) {
+                if (!swiper || swiper.destroyed || !swiper.wrapperEl) return;
+                if (e.target !== swiper.wrapperEl) return;
+                swiper.wrapperEl.removeEventListener("transitionend", onTransitionEnd);
+                if (pausedByPointerEnter || e.detail && e.detail.bySwiperTouchMove) return;
+                resume();
+            }
+            const calcTimeLeft = () => {
+                if (swiper.destroyed || !swiper.autoplay.running) return;
+                if (swiper.autoplay.paused) wasPaused = true; else if (wasPaused) {
+                    autoplayDelayCurrent = autoplayTimeLeft;
+                    wasPaused = false;
+                }
+                const timeLeft = swiper.autoplay.paused ? autoplayTimeLeft : autoplayStartTime + autoplayDelayCurrent - (new Date).getTime();
+                swiper.autoplay.timeLeft = timeLeft;
+                emit("autoplayTimeLeft", timeLeft, timeLeft / autoplayDelayTotal);
+                raf = requestAnimationFrame((() => {
+                    calcTimeLeft();
+                }));
+            };
+            const getSlideDelay = () => {
+                let activeSlideEl;
+                if (swiper.virtual && swiper.params.virtual.enabled) activeSlideEl = swiper.slides.find((slideEl => slideEl.classList.contains("swiper-slide-active"))); else activeSlideEl = swiper.slides[swiper.activeIndex];
+                if (!activeSlideEl) return;
+                const currentSlideDelay = parseInt(activeSlideEl.getAttribute("data-swiper-autoplay"), 10);
+                return currentSlideDelay;
+            };
+            const run = delayForce => {
+                if (swiper.destroyed || !swiper.autoplay.running) return;
+                cancelAnimationFrame(raf);
+                calcTimeLeft();
+                let delay = typeof delayForce === "undefined" ? swiper.params.autoplay.delay : delayForce;
+                autoplayDelayTotal = swiper.params.autoplay.delay;
+                autoplayDelayCurrent = swiper.params.autoplay.delay;
+                const currentSlideDelay = getSlideDelay();
+                if (!Number.isNaN(currentSlideDelay) && currentSlideDelay > 0 && typeof delayForce === "undefined") {
+                    delay = currentSlideDelay;
+                    autoplayDelayTotal = currentSlideDelay;
+                    autoplayDelayCurrent = currentSlideDelay;
+                }
+                autoplayTimeLeft = delay;
+                const speed = swiper.params.speed;
+                const proceed = () => {
+                    if (!swiper || swiper.destroyed) return;
+                    if (swiper.params.autoplay.reverseDirection) {
+                        if (!swiper.isBeginning || swiper.params.loop || swiper.params.rewind) {
+                            swiper.slidePrev(speed, true, true);
+                            emit("autoplay");
+                        } else if (!swiper.params.autoplay.stopOnLastSlide) {
+                            swiper.slideTo(swiper.slides.length - 1, speed, true, true);
+                            emit("autoplay");
+                        }
+                    } else if (!swiper.isEnd || swiper.params.loop || swiper.params.rewind) {
+                        swiper.slideNext(speed, true, true);
+                        emit("autoplay");
+                    } else if (!swiper.params.autoplay.stopOnLastSlide) {
+                        swiper.slideTo(0, speed, true, true);
+                        emit("autoplay");
+                    }
+                    if (swiper.params.cssMode) {
+                        autoplayStartTime = (new Date).getTime();
+                        requestAnimationFrame((() => {
+                            run();
+                        }));
+                    }
+                };
+                if (delay > 0) {
+                    clearTimeout(timeout);
+                    timeout = setTimeout((() => {
+                        proceed();
+                    }), delay);
+                } else requestAnimationFrame((() => {
+                    proceed();
+                }));
+                return delay;
+            };
+            const start = () => {
+                autoplayStartTime = (new Date).getTime();
+                swiper.autoplay.running = true;
+                run();
+                emit("autoplayStart");
+            };
+            const stop = () => {
+                swiper.autoplay.running = false;
+                clearTimeout(timeout);
+                cancelAnimationFrame(raf);
+                emit("autoplayStop");
+            };
+            const pause = (internal, reset) => {
+                if (swiper.destroyed || !swiper.autoplay.running) return;
+                clearTimeout(timeout);
+                if (!internal) pausedByInteraction = true;
+                const proceed = () => {
+                    emit("autoplayPause");
+                    if (swiper.params.autoplay.waitForTransition) swiper.wrapperEl.addEventListener("transitionend", onTransitionEnd); else resume();
+                };
+                swiper.autoplay.paused = true;
+                if (reset) {
+                    if (slideChanged) autoplayTimeLeft = swiper.params.autoplay.delay;
+                    slideChanged = false;
+                    proceed();
+                    return;
+                }
+                const delay = autoplayTimeLeft || swiper.params.autoplay.delay;
+                autoplayTimeLeft = delay - ((new Date).getTime() - autoplayStartTime);
+                if (swiper.isEnd && autoplayTimeLeft < 0 && !swiper.params.loop) return;
+                if (autoplayTimeLeft < 0) autoplayTimeLeft = 0;
+                proceed();
+            };
+            const resume = () => {
+                if (swiper.isEnd && autoplayTimeLeft < 0 && !swiper.params.loop || swiper.destroyed || !swiper.autoplay.running) return;
+                autoplayStartTime = (new Date).getTime();
+                if (pausedByInteraction) {
+                    pausedByInteraction = false;
+                    run(autoplayTimeLeft);
+                } else run();
+                swiper.autoplay.paused = false;
+                emit("autoplayResume");
+            };
+            const onVisibilityChange = () => {
+                if (swiper.destroyed || !swiper.autoplay.running) return;
+                const document = ssr_window_esm_getDocument();
+                if (document.visibilityState === "hidden") {
+                    pausedByInteraction = true;
+                    pause(true);
+                }
+                if (document.visibilityState === "visible") resume();
+            };
+            const onPointerEnter = e => {
+                if (e.pointerType !== "mouse") return;
+                pausedByInteraction = true;
+                pausedByPointerEnter = true;
+                if (swiper.animating || swiper.autoplay.paused) return;
+                pause(true);
+            };
+            const onPointerLeave = e => {
+                if (e.pointerType !== "mouse") return;
+                pausedByPointerEnter = false;
+                if (swiper.autoplay.paused) resume();
+            };
+            const attachMouseEvents = () => {
+                if (swiper.params.autoplay.pauseOnMouseEnter) {
+                    swiper.el.addEventListener("pointerenter", onPointerEnter);
+                    swiper.el.addEventListener("pointerleave", onPointerLeave);
+                }
+            };
+            const detachMouseEvents = () => {
+                if (swiper.el && typeof swiper.el !== "string") {
+                    swiper.el.removeEventListener("pointerenter", onPointerEnter);
+                    swiper.el.removeEventListener("pointerleave", onPointerLeave);
+                }
+            };
+            const attachDocumentEvents = () => {
+                const document = ssr_window_esm_getDocument();
+                document.addEventListener("visibilitychange", onVisibilityChange);
+            };
+            const detachDocumentEvents = () => {
+                const document = ssr_window_esm_getDocument();
+                document.removeEventListener("visibilitychange", onVisibilityChange);
+            };
+            on("init", (() => {
+                if (swiper.params.autoplay.enabled) {
+                    attachMouseEvents();
+                    attachDocumentEvents();
+                    start();
+                }
+            }));
+            on("destroy", (() => {
+                detachMouseEvents();
+                detachDocumentEvents();
+                if (swiper.autoplay.running) stop();
+            }));
+            on("_freeModeStaticRelease", (() => {
+                if (pausedByTouch || pausedByInteraction) resume();
+            }));
+            on("_freeModeNoMomentumRelease", (() => {
+                if (!swiper.params.autoplay.disableOnInteraction) pause(true, true); else stop();
+            }));
+            on("beforeTransitionStart", ((_s, speed, internal) => {
+                if (swiper.destroyed || !swiper.autoplay.running) return;
+                if (internal || !swiper.params.autoplay.disableOnInteraction) pause(true, true); else stop();
+            }));
+            on("sliderFirstMove", (() => {
+                if (swiper.destroyed || !swiper.autoplay.running) return;
+                if (swiper.params.autoplay.disableOnInteraction) {
+                    stop();
+                    return;
+                }
+                isTouched = true;
+                pausedByTouch = false;
+                pausedByInteraction = false;
+                touchStartTimeout = setTimeout((() => {
+                    pausedByInteraction = true;
+                    pausedByTouch = true;
+                    pause(true);
+                }), 200);
+            }));
+            on("touchEnd", (() => {
+                if (swiper.destroyed || !swiper.autoplay.running || !isTouched) return;
+                clearTimeout(touchStartTimeout);
+                clearTimeout(timeout);
+                if (swiper.params.autoplay.disableOnInteraction) {
+                    pausedByTouch = false;
+                    isTouched = false;
+                    return;
+                }
+                if (pausedByTouch && swiper.params.cssMode) resume();
+                pausedByTouch = false;
+                isTouched = false;
+            }));
+            on("slideChange", (() => {
+                if (swiper.destroyed || !swiper.autoplay.running) return;
+                slideChanged = true;
+            }));
+            Object.assign(swiper.autoplay, {
+                start,
+                stop,
+                pause,
+                resume
+            });
+        }
         function Thumb(_ref) {
             let {swiper, extendParams, on} = _ref;
             extendParams({
@@ -5904,100 +6156,51 @@
             });
         }
         function initSliders() {
-            var productSliders = document.querySelectorAll(".product__slider");
-            if (productSliders) productSliders.forEach((function(productSlider) {
-                new swiper_core_Swiper(productSlider, {
-                    modules: [ Navigation, freeMode, Mousewheel ],
-                    observer: true,
-                    observeParents: true,
-                    spaceBetween: 0,
-                    speed: 800,
-                    freeMode: {
-                        enabled: true,
-                        sticky: false,
-                        momentumBounce: false
-                    },
-                    mousewheel: {
-                        enabled: true,
-                        sensitivity: .2,
-                        forceToAxis: true
-                    },
-                    navigation: {
-                        prevEl: ".product__swiper-button-prev",
-                        nextEl: ".product__swiper-button-next"
-                    },
-                    breakpoints: {
-                        0: {
-                            slidesPerView: 1.05,
-                            spaceBetween: 16,
-                            slidesPerGroup: 1,
-                            freeMode: {
-                                enabled: true,
-                                sticky: true,
-                                momentumBounce: false
-                            }
-                        },
-                        768: {
-                            slidesPerView: 2.1,
-                            slidesPerGroup: 2,
-                            spaceBetween: 20
-                        },
-                        992: {
-                            slidesPerView: 3.95,
-                            spaceBetween: 20,
-                            slidesPerGroup: 3
+            var blogSlider = document.querySelector(".blog__slider");
+            if (blogSlider) new swiper_core_Swiper(blogSlider, {
+                modules: [ Navigation, freeMode, Mousewheel ],
+                observer: true,
+                observeParents: true,
+                spaceBetween: 0,
+                speed: 800,
+                freeMode: {
+                    enabled: true,
+                    sticky: false,
+                    momentumBounce: false
+                },
+                mousewheel: {
+                    enabled: true,
+                    sensitivity: .2,
+                    forceToAxis: true
+                },
+                navigation: {
+                    prevEl: ".blog__swiper-button-prev",
+                    nextEl: ".blog__swiper-button-next"
+                },
+                breakpoints: {
+                    0: {
+                        slidesPerView: 1.05,
+                        spaceBetween: 16,
+                        slidesPerGroup: 1,
+                        freeMode: {
+                            enabled: true,
+                            sticky: true,
+                            momentumBounce: false
                         }
                     },
-                    on: {}
-                });
-            }));
-            var blogSliders = document.querySelectorAll(".blog__slider");
-            if (blogSliders) blogSliders.forEach((function(blogSlider) {
-                new swiper_core_Swiper(blogSlider, {
-                    modules: [ Navigation, freeMode, Mousewheel ],
-                    observer: true,
-                    observeParents: true,
-                    spaceBetween: 0,
-                    speed: 800,
-                    freeMode: {
-                        enabled: true,
-                        sticky: false,
-                        momentumBounce: false
+                    768: {
+                        slidesPerView: 2.1,
+                        slidesPerGroup: 2,
+                        spaceBetween: 20
                     },
-                    mousewheel: {
-                        enabled: true,
-                        sensitivity: .2,
-                        forceToAxis: true
-                    },
-                    navigation: {
-                        prevEl: ".blog__swiper-button-prev",
-                        nextEl: ".blog__swiper-button-next"
-                    },
-                    breakpoints: {
-                        0: {
-                            slidesPerView: 1.05,
-                            spaceBetween: 16,
-                            slidesPerGroup: 1,
-                            freeMode: {
-                                enabled: true,
-                                sticky: true,
-                                momentumBounce: false
-                            }
-                        },
-                        768: {
-                            slidesPerView: 2.1,
-                            slidesPerGroup: 2,
-                            spaceBetween: 20
-                        },
-                        992: {
-                            slidesPerView: 2.95,
-                            spaceBetween: 20,
-                            slidesPerGroup: 3
-                        }
-                    },
-                    on: {}
-                });
-            }));
+                    992: {
+                        slidesPerView: 2.95,
+                        spaceBetween: 20,
+                        slidesPerGroup: 3
+                    }
+                },
+                on: {}
+            });
             if (document.querySelector(".product-gallery__slider")) {
                 let galleryMain;
                 let galleryThumbs;
@@ -6092,6 +6295,29 @@
                     }
                 });
             }));
+            blogSlider = document.querySelector(".main__slider");
+            if (blogSlider) new swiper_core_Swiper(blogSlider, {
+                modules: [ Navigation, Pagination, EffectFade, Autoplay ],
+                observer: true,
+                observeParents: true,
+                spaceBetween: 0,
+                speed: 400,
+                effect: "fade",
+                slidesPerView: 1,
+                spaceBetween: 10,
+                loop: true,
+                autoplay: {
+                    delay: 3e3
+                },
+                navigation: {
+                    prevEl: ".swiper-button-prev",
+                    nextEl: ".swiper-button-next"
+                },
+                pagination: {
+                    el: ".swiper-pagination"
+                },
+                on: {}
+            });
         }
         window.addEventListener("DOMContentLoaded", (function(e) {
             initSliders();
